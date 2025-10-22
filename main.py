be02d1e70e1575f2a66767703f1ea9c3cb68bb0f
henry43171 <hukirsa236875@gmail.com>
2025-10-22 22:31:52 +0800

    feat: add batch renaming functionality with prefix and sequence
    - Implemented `batch_rename` to rename all images in a folder
    - Supports custom prefix and zero-padded sequential numbering
    - Returns List[Tuple[str, Image.Image]]

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import grey_quantize
 import compress
 import convert_icon
 import convert
+import renamer
 
 def main():
     # 設定參數
     input_folder = "input_img"
     output_folder = "output_img"
-    mode_status = "convert"
+    mode_status = "rename"
 
     # 讀取圖片
     paths = load_img.list_image_files(input_folder)
@@ -30,7 +31,15 @@ def main():
         # 存檔
         load_img.save_images(quantized_images, input_folder, output_folder)
         print(f"圖片轉換結束")
-    
+
+    elif mode_status == "rename":
+        # 改名
+        quantized_images = renamer.batch_rename("input_img", prefix="img4545_", start_index=1)
+        # 存檔
+        load_img.save_images(quantized_images, input_folder, output_folder)
+        print(f"圖片轉換結束")
+
+
     elif mode_status == "icon":
         # 轉icon
         quantized_images = convert_icon.convert_images_to_icon(input_folder, output_folder, icon_sizes=[(32, 32)])
@@ -38,6 +47,7 @@ def main():
     elif mode_status == "convert":
         # 轉檔
         quantized_images = convert.convert_images_in_folder(input_folder, output_folder, "JPEG")
+    
 
 
 
